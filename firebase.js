788e00c1c90e196092a8e43869f1133b335cfe48
NeverPlayz7 <ahmedboss8603@gmail.com>
2026-02-06 10:42:49 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Firebase CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export function googleLogin(){
+  signInWithPopup(auth, new GoogleAuthProvider())
+    .then(res => alert("Welcome " + res.user.displayName));
+}
+
+export function facebookLogin(){
+  signInWithPopup(auth, new FacebookAuthProvider())
+    .then(res => alert("Welcome " + res.user.displayName));
+}
