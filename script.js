20f2191f979b54b1f58ba1d94afaaffcd8a22ba2
NeverPlayz7 <ahmedboss8603@gmail.com>
2026-02-06 10:48:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function checkout(){
-  fetch("http://localhost:4242/checkout",{
-    method:"POST"
-  })
-  .then(res=>res.json())
-  .then(data=>{
-    window.location = data.url;
-  })
-  .catch(err=>{
-    alert("Stripe server not running");
-  });
-}
+console.log("Oasis Store Loaded Successfully 🚀");
